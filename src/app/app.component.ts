c01c99164b66ce69d78033eead4af5ac52ca8b8a
AlvesM97 <gutomorais2002@gmail.com>
2023-04-23 14:52:40 -0300

    Update src/app/app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { TransferenciaService } from './services/transferencia.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ByteBank_Alura';
+
+  constructor(private service: TransferenciaService) {}
+
 }
